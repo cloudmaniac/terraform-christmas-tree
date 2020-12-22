b0c488cf4d1bb287ec86a087f64b8b2498e32156
Romain Decker <deckerromain@gmail.com>
2020-12-22 20:02:33 +0100

    2020 Christmas Tree

--- main.tf
+++ main.tf
@@ -1,34 +1,20 @@
 ## Terraform Initialization
-# 1/ Forcing to Terraform 0.13
-# 2/ Defining the source for the providers (and potentially their versions)
-# 3/ Defining the backend if not local
-
 terraform {
   required_version = ">= 0.13"
 
-  #   required_providers {
-  #     aws = {
-  #       source = "hashicorp/aws"
-  #     }
-  #   }
-
-  #   backend "remote" {
-  #     hostname     = "app.terraform.io"
-  #     organization = "cloudmaniac"
-
-  #     workspaces {
-  #       name = "cloudmaniac-prod"
-  #     }
-  #   }
+  required_providers {
+    christmas-tree = {
+      source = "cappyzawa/christmas-tree"
+      version = "0.2.2"
+    }
+  }
 }
 
+provider "christmas-tree" {}
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "christmas-tree" "xmas_2020" {
+  path        = "christmas-tree-2020"
+  ball_color  = "white"
+  star_color  = "green"
+  light_color = "red"
 }
